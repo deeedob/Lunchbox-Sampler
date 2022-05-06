c9c92cf433ca781b801ebee1564358b5ce0c53c1
doberst <dennis.oberst@smail.th-koeln.de>
2022-05-06 22:52:26 +0200

    ...

--- code/mhs/src/main.cpp
+++ code/mhs/src/main.cpp
@@ -1,11 +1,20 @@
 #include <ui.hpp>
+#include <define_t40.hpp>
+
+
 
 void setup() {
     Serial.begin(9600);
     Serial.println("<<<<<<<<<<<<Begin program>>>>>>>>>>>>>>>");
-    UI ui;
-    ui.start();
 }
 
 void loop() {
+    Serial.print("POT0 : ");
+    Serial.println(analogRead(POT0));
+    Serial.print("POT1 : ");
+    Serial.println(analogRead(POT1));
+    Serial.print("POT2 : ");
+    Serial.println(analogRead(POT2));
+    Serial.print("POT3 : ");
+    Serial.println(analogRead(POT3));
 }
