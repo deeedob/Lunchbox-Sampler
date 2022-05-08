a94f234a18cd39e6459b2f07740a45f7997e4024
doberst <dennis.oberst@smail.th-koeln.de>
2022-05-09 01:20:36 +0200

    interrupt rotary

--- code/mhs/src/main.cpp
+++ code/mhs/src/main.cpp
@@ -1,20 +1,60 @@
-#include <ui.hpp>
 #include <define_t40.hpp>
+#include <ADC.h>
+#include <ADC_Module.h>
+#include <Bounce.h>
 
-
+Bounce b(ROTARY_SW, 10);
+ADC adc;
+int pinAstateCurrent = LOW;
+int pinAStateLast = pinAstateCurrent;
 
 void setup() {
     Serial.begin(9600);
     Serial.println("<<<<<<<<<<<<Begin program>>>>>>>>>>>>>>>");
+    pinMode(ROTARY_SW, INPUT_PULLUP);
+    pinMode(ROTARY_A, INPUT_PULLUP);
+    pinMode(ROTARY_B, INPUT_PULLUP);
+
+    attachInterrupt(digitalPinToInterrupt(ROTARY_SW), []() {
+        b.update();
+        if(b.fallingEdge()) {
+            Serial.println("Interrup press");
+            Serial.println(adc.analogRead(POT0));
+        }
+
+        if(b.risingEdge()) {
+            Serial.println("Interrup release");
+            Serial.println();
+        }
+    }, CHANGE);
+
+    attachInterrupt(digitalPinToInterrupt(ROTARY_B), []() {
+        pinAstateCurrent = digitalRead(ROTARY_A);
+        if((pinAStateLast == LOW) && (pinAstateCurrent == HIGH)) {
+            if( digitalReadFast(ROTARY_B) == HIGH) {
+                Serial.println("Left");
+            } else {
+                Serial.println("Right");
+            }
+        }
+        pinAStateLast = pinAstateCurrent;
+    }, CHANGE);
+
+    adc.adc0->enableInterrupts([]() {
+        Serial.println("adc0 interrupted!");
+    });
+    adc.adc1->enableInterrupts([]() {
+        Serial.println("adc1 interrupted!");
+    });
+
 }
 
 void loop() {
-    Serial.print("POT0 : ");
-    Serial.println(analogRead(POT0));
-    Serial.print("POT1 : ");
-    Serial.println(analogRead(POT1));
-    Serial.print("POT2 : ");
-    Serial.println(analogRead(POT2));
-    Serial.print("POT3 : ");
-    Serial.println(analogRead(POT3));
+
+    // A0 = 14 A1= 15,A3=24, A4=26
+
+    //noInterrupts();
+    /* interrupts disabled for critical section */
+    //Serial.println("uninterruptable code");
+    //interrupts(); // re-enable interrupts
 }
