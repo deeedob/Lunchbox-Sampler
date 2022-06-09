091e783eadec13ca70d7eecf701154b7b94b2b55
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-06-09 15:55:22 +0200

    Installing missing libraries and cleaning up headers

--- code/mhs/src/test.cpp
+++ code/mhs/src/test.cpp
@@ -1,15 +1,11 @@
 #include <Arduino.h>
-#include <Audio.h>
-#include <Wire.h>
 #include <SD.h>
 #include <SPI.h>
 #include <SerialFlash.h>
-#include <Adafruit_SSD1327.h>
-#include <Encoder.h>
 #include <Bounce.h>
 #include <iostream>
-#include <fstream>
 
+#include "memory.hpp"
 
 using namespace std;
 
@@ -18,21 +14,26 @@ using namespace std;
 #define SDCARD_SCK_PIN  13
 #define FLASH_PIN 6
 
-
 String *settings;
 
 int searchFreeMidi();
+
+void directoryListingFlash();
+
 bool loadSamplePack(const char*);
-bool loadSample(const char*);
-bool deleteSampleFromFlash(const char*);
 bool deleteSamplePackFromFlash(const char* path);
+
+bool loadSample(const char*);
+SerialFlashFile triggerSample(const char* path);
+bool replaceSample(const char* oldPath, const char* newPath);
+bool deleteSampleFromFlash(const char*);
+
 void deleteAllFilesOnFlash();
+
 void readSettingsTxt();
 void updateSettingsTxt();
-bool replaceSample(const char* oldPath, const char* newPath);
-SerialFlashFile triggerSample(const char* path);
+
 void configSettings();
-void directoryListingFlash();
 bool compareFiles(File &file, SerialFlashFile &ffile);
 SerialFlashFile file;
 
@@ -58,117 +59,46 @@ void setup() {
     directoryListingFlash();
     readSettingsTxt();
 }
+
 void loop() {
 
 }
-void readSettingsTxt()
-{
-    if (!SD.begin(SDCARD_CS_PIN )) {
-        Serial.println("initialization failed!");
-        return;
-    }
-    File myFile;
-    myFile = SD.open("settings.txt", FILE_READ);
-    if(myFile) {
-        Serial.println("settings.txt");
-        while (myFile.available()) {
-            Serial.write(myFile.read());
-        }
-        // close the file:
-        myFile.close();
-    }
-    else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-    }
-}
-void updateSettingsTxt()
-{
-    if (!SD.begin(SDCARD_CS_PIN )) {
-        Serial.println("initialization failed!");
-        return;
-    }
-    File myFile;
-    SD.remove("settings.txt");
-    myFile = SD.open("settings.txt", FILE_WRITE);
-    if (myFile) {
-        for(int i=0; i<128; i++)
-        {
-            myFile.print(i);
-            myFile.print(",");
-            myFile.print(settings[i]);
-            myFile.print(";");
-        }
-        myFile.close();
-        Serial.println("done.");
-        configSettings();
-    }
-    else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-    }
-}
-int searchFreeMidi(){
-    for(int i=0;i<128;i++){
-        if (settings[i].equals("not defined")){
-            return i;
-        }
-    }
-    return 128;
-}
-void configSettings(){
-    if (!SD.begin(SDCARD_CS_PIN )) {
-        Serial.println("initialization failed!");
-        return;
-    }
-    File myFile;
-    myFile = SD.open("settings.txt", FILE_READ);
-    if(myFile) {
-         int count = 0;
-            char buf1[myFile.size()];
-            myFile.read(buf1, myFile.size());
-            while (1) {
-                int i = 0;
-                String name;
-                String midiValue;
-                    while (buf1[count + i] != ',') {
-                        midiValue.append(buf1[i + count]);
-                        i++;
-                    }
-                    i++;
-                    while (buf1[count + i] != ';') {
-                        name += buf1[i + count];
-                        i++;
-                    }
-                    i++;
-                    settings[midiValue.toInt()] = name;
-                    count += i;
-                    if(count==myFile.size())break;
-            }
-            myFile.close();
-            return;
-    }
-    else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening test.txt");
-    }
-}
-void deleteAllFilesOnFlash()
-{
-    if (!SerialFlash.begin(FLASH_PIN)) {
-        while (1) {
-            Serial.println(F("Unable to access SPI Flash chip"));
-            return;
-        }
-    }
-    Serial.println("deleting all Files. Please wait until 'ready' (ca.40s)");
-    SerialFlash.eraseAll();
-    while (SerialFlash.ready() == false)
-    {
 
+void directoryListingFlash()
+{
+    while (!Serial);
+    delay(100);
+    Serial.println(F("All Files on SPI Flash chip:"));
+
+    if (!SerialFlash.begin(FLASH_PIN)) {
     }
-    Serial.println("ready");
+
+    int count=0;
+    SerialFlash.opendir();
+    while (1) {
+        char filename[64];
+        uint32_t filesize;
+
+        if (SerialFlash.readdir(filename, sizeof(filename), filesize)) {
+            Serial.print(F("  "));
+            Serial.print(filename);
+            Serial.print(F("  "));
+            Serial.print(filesize);
+            Serial.print(F(" bytes"));
+            Serial.println();
+            count++;
+        }
+        else {
+            if(count==0)
+            {
+                Serial.println("SPI Flash chip is empty");
+            }
+            break;
+        }
+    }
+
 }
+
 // load Sample Pack from SD to Flash
 bool loadSamplePack(const char* path)
 {
@@ -238,6 +168,55 @@ bool loadSamplePack(const char* path)
     dir.close();
     return true;
 }
+
+bool deleteSamplePackFromFlash(const char* path)
+{
+    if (!SD.begin(SDCARD_CS_PIN)) {
+        Serial.println("Unable to access SPI Flash chip");
+        return false;
+    }
+    if (!SerialFlash.begin(FLASH_PIN)) {
+        Serial.println("Unable to access SPI Flash chip");
+        return false;
+    }
+    File dir=SD.open(path);
+    while (1) {
+        File f = dir.openNextFile();
+        if (!f) break;
+        const char *filename = f.name();
+        unsigned long length = f.size();
+        if (SerialFlash.exists(filename)) {
+            Serial.println("file exists");
+            if(SerialFlash.remove(filename))
+            {
+                Serial.println(filename);
+                Serial.println( "is deleted");
+                for(int i=0; i<128; i++)
+                {
+                    if(settings[i].equals(filename))
+                    {
+                        settings[i]="not defined";
+                        updateSettingsTxt();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Serial.println( "unable to delete ");
+                Serial.println(filename);
+            }
+        }
+        else
+        {
+            Serial.println(filename);
+            Serial.println( " does not exist on Flash");
+        }
+        f.close();
+    }
+    return true;
+}
+
 bool loadSample(const char* path)
 {
     if (!SD.begin(SDCARD_CS_PIN)) {
@@ -291,6 +270,24 @@ bool loadSample(const char* path)
     }
     return false;
 }
+
+SerialFlashFile triggerSample(const char* path)
+{
+    if(!SerialFlash.begin(FLASH_PIN))
+    {
+        Serial.println("Couldnt open SerialFlash");
+    }
+    SerialFlashFile ff = SerialFlash.open(path);
+    return ff;
+
+}
+
+bool replaceSample(const char* oldPath, const char* newPath)
+{
+
+    return true;
+}
+
 bool deleteSampleFromFlash(const char* name)
 {
     if (!SD.begin(SDCARD_CS_PIN)) {
@@ -320,101 +317,121 @@ bool deleteSampleFromFlash(const char* name)
     Serial.println(name);
     return false;
 }
-bool deleteSamplePackFromFlash(const char* path)
+
+
+void deleteAllFilesOnFlash()
 {
-    if (!SD.begin(SDCARD_CS_PIN)) {
-        Serial.println("Unable to access SPI Flash chip");
-        return false;
-    }
     if (!SerialFlash.begin(FLASH_PIN)) {
-        Serial.println("Unable to access SPI Flash chip");
-        return false;
-    }
-    File dir=SD.open(path);
-    while (1) {
-        File f = dir.openNextFile();
-        if (!f) break;
-        const char *filename = f.name();
-        unsigned long length = f.size();
-        if (SerialFlash.exists(filename)) {
-            Serial.println("file exists");
-            if(SerialFlash.remove(filename))
-            {
-                Serial.println(filename);
-                Serial.println( "is deleted");
-                for(int i=0; i<128; i++)
-                {
-                    if(settings[i].equals(filename))
-                    {
-                        settings[i]="not defined";
-                        updateSettingsTxt();
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                Serial.println( "unable to delete ");
-                Serial.println(filename);
-            }
-        }
-        else
-        {
-            Serial.println(filename);
-            Serial.println( " does not exist on Flash");
-        }
-        f.close();
-    }
-    return true;
-}
-bool replaceSample(const char* oldPath, const char* newPath)
-{
-
-    return true;
-}
-void directoryListingFlash()
-{
-    while (!Serial) ;
-    delay(100);
-    Serial.println(F("All Files on SPI Flash chip:"));
-
-    if (!SerialFlash.begin(FLASH_PIN)) {
-    }
-    int count=0;
-    SerialFlash.opendir();
-    while (1) {
-        char filename[64];
-        uint32_t filesize;
-
-        if (SerialFlash.readdir(filename, sizeof(filename), filesize)) {
-            Serial.print(F("  "));
-            Serial.print(filename);
-            Serial.print(F("  "));
-            Serial.print(filesize);
-            Serial.print(F(" bytes"));
-            Serial.println();
-            count++;
-        }
-        else {
-            if(count==0)
-            {
-                Serial.println("SPI Flash chip is empty");
-            }
-            break;
+        while (1) {
+            Serial.println(F("Unable to access SPI Flash chip"));
+            return;
         }
     }
-
-}
-SerialFlashFile triggerSample(const char* path)
-{
-    if(!SerialFlash.begin(FLASH_PIN))
+    Serial.println("deleting all Files. Please wait until 'ready' (ca.40s)");
+    SerialFlash.eraseAll();
+    while (SerialFlash.ready() == false)
     {
-        Serial.println("Couldnt open SerialFlash");
-    }
-    SerialFlashFile ff = SerialFlash.open(path);
-    return ff;
 
+    }
+    Serial.println("ready");
 }
+
+void readSettingsTxt()
+{
+    if (!SD.begin(SDCARD_CS_PIN )) {
+        Serial.println("initialization failed!");
+        return;
+    }
+    File myFile;
+    myFile = SD.open("settings.txt", FILE_READ);
+    if(myFile) {
+        Serial.println("settings.txt");
+        while (myFile.available()) {
+            Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+    }
+    else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
+void updateSettingsTxt()
+{
+    if (!SD.begin(SDCARD_CS_PIN )) {
+        Serial.println("initialization failed!");
+        return;
+    }
+    File myFile;
+    SD.remove("settings.txt");
+    myFile = SD.open("settings.txt", FILE_WRITE);
+    if (myFile) {
+        for(int i=0; i<128; i++)
+        {
+            myFile.print(i);
+            myFile.print(",");
+            myFile.print(settings[i]);
+            myFile.print(";");
+        }
+        myFile.close();
+        Serial.println("done.");
+        configSettings();
+    }
+    else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
+int searchFreeMidi(){
+    for(int i=0;i<128;i++){
+        if (settings[i].equals("not defined")){
+            return i;
+        }
+    }
+    return 128;
+}
+
+void configSettings(){
+    if (!SD.begin(SDCARD_CS_PIN )) {
+        Serial.println("initialization failed!");
+        return;
+    }
+    File myFile;
+    myFile = SD.open("settings.txt", FILE_READ);
+    if(myFile) {
+         int count = 0;
+            char buf1[myFile.size()];
+            myFile.read(buf1, myFile.size());
+            while (1) {
+                int i = 0;
+                String name;
+                String midiValue;
+                    while (buf1[count + i] != ',') {
+                        midiValue.append(buf1[i + count]);
+                        i++;
+                    }
+                    i++;
+                    while (buf1[count + i] != ';') {
+                        name += buf1[i + count];
+                        i++;
+                    }
+                    i++;
+                    settings[midiValue.toInt()] = name;
+                    count += i;
+                    if(count==myFile.size())break;
+            }
+            myFile.close();
+            return;
+    }
+    else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
 bool compareFiles(File &file, SerialFlashFile &ffile) {
     file.seek(0);
     ffile.seek(0);
