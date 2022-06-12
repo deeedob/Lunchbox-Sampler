b3580e56fd6f071f24a58f58fc006a47c3fac180
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-06-12 21:43:51 +0200

    Rewriting test.cpp to fit with newly created memory classes

--- code/mhs/src/test.cpp
+++ code/mhs/src/test.cpp
@@ -5,7 +5,8 @@
 #include <Bounce.h>
 #include <iostream>
 
-#include "memory.hpp"
+#include "MemGeneric.hpp"
+#include "MemSample.hpp"
 
 using namespace std;
 
@@ -20,15 +21,16 @@ void setup() {
     // Settings zieht sich in der Setup Methode über configSettings() die Midi Zuweisungen aus der txt Datei, welche wiederum in der Laufzeit von settings aktualisiert wird über
     // updateSettingsTxt(). Werden neue Samples/Samplepacks hinzugefuegt werden sie zunächst ins Array eingelesen und dann in der Textdatei aktualisiert.
     // Gleiches gilt für das Löschen von Samples/SamplePacks. Über readSettingsTxt() kann der Inhalt der TextDatei gelesen werden.
+    MemSample *msamp = MemSample::getInstance();
+    MemGeneric *mgen = MemGeneric::getInstance();
 
-    Memory::configSettings();
-    //Memory::loadSamplePack("SamplePack01");
-    //Memory::deleteSamplePack("SamplePack01");
-    //Memory::loadSample("SamplePack01/Rim.wav");
-    //Memory::deleteSample("Rim.wav");
-    //Memory::purgeFlash();
-    Memory::listFlash();
-    Memory::readSettings();
+    msamp->loadSamplePack("SamplePack01");
+    msamp->deleteSamplePack("SamplePack01");
+    msamp->loadSample("SamplePack01/Rim.wav");
+    msamp->deleteSample("Rim.wav");
+    mgen->purgeFlash();
+    mgen->listFlash();
+    mgen->readSettings();
 }
 
 void loop() {
