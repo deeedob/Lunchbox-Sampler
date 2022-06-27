c363272243f8bacaca81ea1cefa172d63bf5901b
Dennis Oberst <dennis.ob@protonmail.com>
2022-06-27 23:08:12 +0200

    multiplexer test

--- code/mhs/src/test.cpp
+++ code/mhs/src/test.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
+#include <define_t40.hpp>
 
+/* Arduino ... why u do this shit?! */
 #undef main
 
 int main() {
     Serial.begin(9600);
     Serial.println(":::Beginning Multiplexer Test:::");
 
+    while(true) {
+
+    }
 
     return 0;
 }
