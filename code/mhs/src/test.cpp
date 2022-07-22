9158e31342c9fae160b0f7d681fb9874843f288d
lWilbert2 <73698592+lWilbert2@users.noreply.github.com>
2022-07-22 12:59:54 +0200

    RecordMidi test

--- code/mhs/src/test.cpp
+++ code/mhs/src/test.cpp
@@ -4,53 +4,133 @@
 #include <map>
 #include <MIDIUSB.h>
 #include <MIDIcontroller.h>
+#include <MIDI.h>
 
-/* Arduino ... why u do this shit?! */
 #undef assert
 #undef main
+void sendMidi();
+void DAWTOMidi();
+void MidiIn();
 
-void assert_3(const char *__file, int __lineno, const char *__sexp );
-#define assert( a ) if (!a) { assert_3(__FILE__, __LINE__,  #a); }
+typedef struct {
+    u_int8_t type;
+    u_int8_t channel;
+    u_int8_t data1;
+    u_int8_t data2;
+} midiData;
 
-/* https://github.com/joshnishikawa/MIDIcontroller/commit/f345d2e08b770a9e53a71319b5f620138afb1868
- * some changes happened? what is going on
- * */
-
-int main() {
+/*int main() {
     Serial.begin(115200);
-    //Serial1.begin(2000000);
-    //while(!Serial && millis() < 3000 ){ }
     Serial.println(":::Starting MIDI Test :::");
     while(true) {
-        if(MidiUSB.available()) {
-            auto msg = MidiUSB.read();
-            /* TODO: some Serial ports must be empty for printing -- see @platformio.ini */
-            Serial.print(msg.header);
-            Serial.print(" ");
-            Serial.print(msg.byte1);
-            Serial.print(" ");
-            Serial.print(msg.byte2);
-            Serial.print(" ");
-            Serial.print(msg.byte3);
-            Serial.print(" ");
-        }
-        yield();
+        DAWTOMidi();
     }
 
     return 0;
+}*/
+void DAWTOMidi()
+{
+    while(usbMIDI.read()) {
+        int velocity, channel, note;
+        // Is there a MIDI message incoming ?
+        int type = usbMIDI.getType();
+        Serial.print("type");
+        Serial.println(type);
+        switch (type) {
+            case midi::NoteOn:
+                note = usbMIDI.getData1();
+                velocity = usbMIDI.getData2();
+                channel = usbMIDI.getChannel();
+                if (velocity > 0) {
+                    Serial.println(note);
+                    Serial.println(" ON");
+                } else {
+                    Serial.println(note);
+                    Serial.println(" OFF");
+
+                }
+                break;
+            case midi::NoteOff:
+                note = usbMIDI.getData1();
+                velocity = usbMIDI.getData2();
+                channel = usbMIDI.getChannel();
+                Serial.println(note);
+                Serial.println(" OFF");
+                break;
+        }
+    }
+
+}
+void MidiToDAW(){
+    MIDI_CREATE_INSTANCE(HardwareSerial,Serial1,MIDI)
+    int type, velocity, channel, note;
+
+    if (MIDI.read()) {                    // Is there a MIDI message incoming ?
+        byte type = MIDI.getType();
+        switch (type) {
+            case midi::NoteOn:
+                note = MIDI.getData1();
+                velocity = MIDI.getData2();
+                channel = MIDI.getChannel();
+                if (velocity > 0) {
+                    usbMIDI.sendNoteOn(note, velocity, channel);
+                } else {
+                    usbMIDI.sendNoteOff(note, velocity, channel);
+
+                }
+                break;
+            case midi::NoteOff:
+                note = MIDI.getData1();
+                velocity = MIDI.getData2();
+                channel = MIDI.getChannel();
+                usbMIDI.sendNoteOff(note, velocity, channel);
+                break;
+        }
+    }
+}
+void MidiIn()
+{
+    MIDI_CREATE_INSTANCE(HardwareSerial, Serial1,MIDI)
+    byte type;
+    int velocity, channel,note;
+
+    Serial.println(MIDI.getData1());
+    if (MIDI.read()) {
+        Serial.println("isMIDI");// Is there a MIDI message incoming ?
+        byte type = MIDI.getType();
+        switch (type) {
+            case midi::NoteOn:
+                note = MIDI.getData1();
+                velocity = MIDI.getData2();
+                channel = MIDI.getChannel();
+                if (velocity > 0) {
+                    Serial.println("NOTE ON: ");
+                    Serial.println(note);
+                    Serial.println(velocity);
+                    Serial.println(channel);
+                    Serial.println();
+
+                } else {
+                    Serial.println("NOTE OFF: ");
+                    Serial.println(note);
+                    Serial.println(velocity);
+                    Serial.println(channel);
+                    Serial.println();
+                }
+                break;
+            case midi::NoteOff:
+                note = MIDI.getData1();
+                velocity = MIDI.getData2();
+                channel = MIDI.getChannel();
+                //usbMIDI.sendNoteOff(note, velocity, channel);
+                Serial.println("NOTE OFF: ");
+                Serial.println(note);
+                Serial.println(velocity);
+                Serial.println(channel);
+                Serial.println();
+                break;
+        }
+    }
+
 }
 
-// handle diagnostic informations given by assertion and abort program execution:
-void assert_3(const char *__file, int __lineno, const char *__sexp) {
-    // transmit diagnostic informations through serial link.
-    // Serial.println(__func);
-    Serial.println(" ___ ASSERT FAILED ___ FILE, LINE#, Expression ");
-    Serial.println(__file);
-    Serial.println(__lineno, DEC);
-    Serial.println(__sexp);
-    Serial.flush();
-    // abort program execution.
-    //xxx abort();
-    Serial.println(" ___ ASSERT FAILED ___ STOPPING !!!!");
-    while( 1 );
-}
