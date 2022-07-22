a0d429fa4ecd0ff476ef966b2d3b91eec7b8549e
Dennis Oberst <dennis.ob@protonmail.com>
2022-07-22 17:45:48 +0200

    restored memory branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,44 @@
-#include <lunchbox_sampler.hpp>
+#include <define_t40.hpp>
+#include <SD.h>
+#include <SPI.h>
+#include <SerialFlash.h>
+#include <CSV_Parser.h>
 
-#undef main
+#include <MemFlash.hpp>
+#include <MemSD.hpp>
 
-int main() {
-	LunchboxSampler& sampler = LunchboxSampler::getInstance();
-	sampler.run();
+using namespace lbs;
+
+void setup()
+{
+
+    SPI.setSCK(SDCARD_SCK_PIN);
+    SPI.setMOSI(SDCARD_MOSI_PIN);
+
+    Serial.begin(9600);
+    Serial.println("Starting Memory Class");
+
+    MemFlash &ms = MemFlash::getInstance();
+
+
+    Serial.println("");
+    Serial.println("Printing");
+    ms.loadSamplePack("SamplePack02");
+
+    //MemFlash *msamp = MemFlash::getInstance();
+
+/*
+    msamp->loadSamplePack("SamplePack01");
+    msamp->deleteSamplePack("SamplePack01");
+    msamp->loadSample("SamplePack01/Rim.wav");
+    msamp->deleteSample("Rim.wav");
+    mgen->purgeFlash();
+    mgen->listFlash();
+    msamp->readSettings();
+*/
+}
+
+void loop()
+{
+
 }
