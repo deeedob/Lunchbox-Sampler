f039ea5dd8bed6634be8017d75318f62cc5df30e
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-18 13:55:10 +0200

    remove Singleton

--- lib/memory/MemSample.hpp
+++ lib/memory/MemSample.hpp
@@ -1,4 +1,4 @@
-#pragma once;
+#pragma once
 #include <define_t40.hpp>
 #include "MemFlash.hpp"
 #include "MemSD.hpp"
@@ -6,51 +6,50 @@
 #include "Arduino.h"
 #endif
 
-namespace lbs
-{
-	
-	class MemSample
-	{
-	
-	private:
-		class MidiMapping
-		{
-		
-		public:
-			bool add( std::string pitch, int8_t octave, std::string sample, std::string mode );
-			void removeSample(std::string sampleName);
-			std::string getSampleName(uint8_t midiNote);
-			std::vector< std::string >* getSampleList();
-			MidiMapping();
-		
-		private:
-			enum PlaybackMode
-			{
-				ONESHOT = 0,
-				LOOP = 1,
-				TOGGLE = 2
-			};
-			static inline PlaybackMode getModeFromString( std::string mode );
-			static const std::unordered_map< std::string, uint8_t > pitches;
-			
-			std::string samples[128];
-			PlaybackMode mode[128];
-			
-		};
-	
-	public:
-		static MemSample& getInstance();
-		bool loadSamplePack( const std::string packName );
-		friend void playSample(uint8_t midiNote);
-		void playSample(uint8_t Note);
-	
-	private:
-		std::string currentSamplePack;
-		MidiMapping mapping;
-		lbs::MemFlash& mf = MemFlash::getInstance();
-		lbs::MemSD& ms = MemSD::getInstance();
-		MemSample();
-		
-	};
-	
+namespace lbs {
+
+    class MemSample {
+
+    private:
+        class MidiMapping {
+
+        public:
+            bool add(const std::string &pitch, int8_t octave, std::string sample, const std::string &mode);
+            void removeSample(const std::string &sampleName);
+            std::string getSampleName(uint8_t midiNote);
+            std::vector<std::string> *getSampleList();
+
+            MidiMapping();
+
+        private:
+            enum PlaybackMode {
+                ONESHOT = 0,
+                LOOP = 1,
+                TOGGLE = 2
+            };
+
+            static inline PlaybackMode getModeFromString(const std::string &mode);
+
+            static const std::unordered_map<std::string, uint8_t> pitches;
+
+            std::string samples[128];
+            PlaybackMode mode[128];
+
+        };
+
+    public:
+        bool loadSamplePack(const std::string &packName);
+
+        void playSample(uint8_t midiNote);
+
+    private:
+        std::string currentSamplePack;
+        MidiMapping mapping;
+        lbs::MemFlash &mf = MemFlash::getInstance();
+        lbs::MemSD &ms = MemSD::getInstance();
+
+        MemSample();
+
+    };
+
 }
