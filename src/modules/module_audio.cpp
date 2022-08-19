c97eaa95807594024a0f50120d65d76d08d48db2
lWilbert2 <73698592+lWilbert2@users.noreply.github.com>
2022-08-19 15:26:54 +0200

    Midi Visualisation still static,but display of send in note

--- src/modules/module_audio.cpp
+++ src/modules/module_audio.cpp
@@ -2,6 +2,13 @@
 
 using namespace lbs;
 
+struct MidiData {
+	u_int8_t type;
+	u_int8_t channel;
+	u_int8_t data1;
+	u_int8_t data2;
+};
+
 ModuleAudio::ModuleAudio()
 	: AbstractModule( "Audio" )
 {
@@ -19,6 +26,15 @@ ModuleAudio::~ModuleAudio()
 
 void ModuleAudio::enter( Graphics* g )
 {
+	drawMidiVisualisation({144, 1, 66, 75}, g);
+	g->drawWindow( m_topNav );
+	g->drawWindow( m_midiVisualisation );
+	g->drawWindow( m_information );
+	//g->drawWindow( temp_window );
+	g->display();
+}
+void ModuleAudio::drawMidiVisualisation(MidiData m_midi_data, Graphics* g)
+{
 	Window temp_window;
 	temp_window.fillScreen( 0x44 );
 	temp_window.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
@@ -29,41 +45,63 @@ void ModuleAudio::enter( Graphics* g )
 	m_topNav.setTextColor(0x44);
 	m_topNav.printlnCentered("MIDI");
 	
+	int whiteKeys [14]={1,3,5,6,8,10,12,13,15,17,18,20,22,24};
+	int blackKeys [10]={2,4,7,9,11,14,16,19,21,23};
+	int Key;
+	for(int i=24; i<100; i=i+24)
+	{
+		Serial.println("e");
+		if(m_midi_data.data1>=i &&m_midi_data.data1<i+24)
+		{
+			Key=m_midi_data.data1-i+1;
+			Serial.println(Key);
+		}
+	}
+	int wCount=0;
 	for(int i=8; i<=116; i=i+8)
 	{
-		m_midiVisualisation.drawRect(i,5,8,50,0x44);
-		m_midiVisualisation.fillRect(i+1,6,6,48,0xff);
+		if(whiteKeys[wCount]==Key)
+		{
+			m_midiVisualisation.drawRect(i,5,8,50,0xbb);
+			m_midiVisualisation.fillRect(i+1,6,6,48,0xbb);
+		}
+		else{
+			m_midiVisualisation.drawRect(i,5,8,50,0x44);
+			m_midiVisualisation.fillRect(i+1,6,6,48,0xff);
+		}
+		    wCount++;
 	}
 	int count=0;
+	int bcount=0;
 	for(int i=8 ; i<116; i=i+8)
 	{
 		if(count!=2 && count!=6 && count!=9 &&count!=13)
 		{
-			m_midiVisualisation.drawRect(i+5,5,6,30,0x44);
-			m_midiVisualisation.fillRect(i+6,6,4,28,0x11);
+			if(blackKeys[bcount]==Key)
+			{
+				m_midiVisualisation.drawRect(i+5,5,6,30,0x44);
+				m_midiVisualisation.fillRect(i+6,6,4,28,0xbb);
+			}
+			else
+			{
+				m_midiVisualisation.drawRect(i+5,5,6,30,0x44);
+				m_midiVisualisation.fillRect(i+6,6,4,28,0x11);
+			}
+			bcount++;
 	
 		}
-		Serial.println(count);
 		count++;
 	}
 	m_information.fillScreen(0x44);
 	m_information.drawWindowBorder({ 2, 2 }, 0, 0xbb, 1 );
 	m_information.setTextColor(0xff);
 	m_information.setTextSize(0.4);
-	m_information.printlnHCentered("Note:");
-	m_information.printlnHCentered("Velocity:");
-	
+	m_information.printlnHCentered("Note: ");
+	m_information.printlnHCentered("Velocity: ");
 	
 	temp_window.fillRect(4,80,120,44,0x44);
 	temp_window.drawRect(4,80,120,44,0xbb);
 	temp_window.drawRect(8,84,112,36,0xbb);
-	
-	
-	g->drawWindow( m_topNav );
-	g->drawWindow( m_midiVisualisation );
-	g->drawWindow( m_information );
-	//g->drawWindow( temp_window );
-	g->display();
 }
 void ModuleAudio::update( Graphics* g, Events::DIGITAL e )
 {
--- src/modules/module_audio.hpp
+++ src/modules/module_audio.hpp
@@ -10,9 +10,16 @@ using namespace lbs;
 class ModuleAudio : public AbstractModule
 {
 public:
+	struct MidiData{
+		u_int8_t type;
+		u_int8_t channel;
+		u_int8_t data1;
+		u_int8_t data2;
+	};
 	ModuleAudio();
 	void enter( Graphics* g ) override;
 	void update( Graphics* g, Events::DIGITAL e ) override;
+	void drawMidiVisualisation(MidiData midi_data, Graphics* g);
 	void exit() override;
 	~ModuleAudio() override;
 private:
