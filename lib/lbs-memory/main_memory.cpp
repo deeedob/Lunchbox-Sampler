f21ebcc46c23fb9ed1d35c7ee1d52c7e328dee5f
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-08-20 11:51:03 +0200

    merge mem_flash and mem_sd into one class for now, separation later

--- lib/lbs-memory/main_memory.cpp
+++ lib/lbs-memory/main_memory.cpp
@@ -1,5 +1,96 @@
-/**************************************************************************************************
- * Copyright (c) 2022. Dennis Oberst                                                              *
- **************************************************************************************************/
-
 #include "main_memory.hpp"
+
+namespace lbs
+{
+
+	inline std::string getBasename( const std::string& filepath ) {
+		return filepath.substr( filepath.find_last_of( "/\\" ) + 1 );
+	}
+
+	std::vector< std::string > MainMemory::getAllFromFlash() {
+		unsigned int count = 0;
+		char filename[ 64 ];
+		uint32_t filesize;
+		std::vector< std::string > filelist;
+
+		/* announce reading filelist (prob setting file iterator to 0) */
+		SerialFlash.opendir();
+
+		while( SerialFlash.readdir( filename, sizeof( filename ), filesize ) ) {
+			filelist.emplace_back( filename );
+
+#ifdef VERBOSE
+			Serial.print( "File " );
+			Serial.print( count );
+			Serial.print( F( ": " ) );
+			Serial.print( filename );
+			Serial.print( F( "  " ) );
+			Serial.print( filesize );
+			Serial.print( F( " bytes" ) );
+			Serial.println();
+#endif
+			count++;
+		}
+
+		if( count == 0 ) Serial.println( "Flash is empty" );
+
+		return filelist;
+	}
+
+	void MainMemory::transferSingleToFlash( const std::string& filepath ) {
+
+		Serial.println( "Transfer to Flash function begin" );
+
+		File f = SD.open( filepath.c_str() );
+
+		if( !f ) {
+#ifdef VERBOSE
+			Serial.print( "transferToFlash: error reading file " );
+			Serial.print( filepath.c_str() );
+			Serial.println( " from SD" );
+#endif
+		}
+
+		std::string basename = lbs::getBasename( const_cast< std::string& >( filepath ) );
+
+		if( !SerialFlash.createErasable( basename.c_str(), f.size() ) ) {
+#ifdef VERBOSE
+			Serial.print( "transferToFlash: error creating file" );
+			Serial.print( basename.c_str() );
+			Serial.println( " on Flash" );
+#endif
+		}
+
+		SerialFlashFile ff = SerialFlash.open( basename.c_str() );
+
+		char* buf = new char[ ff.size() ];
+		f.read( buf, ff.size() );
+		ff.write( buf, ff.size() );
+	}
+
+	MainMemory::MainMemory() {
+		SPI.setMOSI( C_SDCARD_MOSI_PIN );
+		SPI.setSCK( C_SDCARD_SCK_PIN );
+
+		if( !SD.begin( C_SDCARD_CS_PIN ) ) { Serial.println( "SD Card: initialization failed!" ); }
+
+		if( !SerialFlash.begin( C_FLASH_PIN ) ) {
+#ifdef VERBOSE
+			Serial.println( "Error initializing Flash Chip!" );
+#endif
+		}
+	}
+
+	void MainMemory::eraseFlash() {
+#ifdef VERBOSE
+		Serial.println( "purgeFlash: deleting all Files. Please wait until 'ready' (ca.40s)" );
+#endif
+		SerialFlash.eraseAll();
+		while( !SerialFlash.ready() ) {}
+
+#ifdef VERBOSE
+		Serial.println( "ready" );
+#endif
+	}
+
+}// namespace lbs
--- lib/lbs-memory/main_memory.hpp
+++ lib/lbs-memory/main_memory.hpp
@@ -1,5 +1,29 @@
 #pragma once
 
-class MainMemory {
+#include "define_t40.hpp"
+#include <SD.h>
+#include <SerialFlash.h>
+#include <string>
+#include <vector>
 
-};
+namespace lbs
+{
+
+	class MainMemory
+	{
+
+	public:
+		MainMemory();
+		static std::vector< std::string > getAllFromFlash();
+
+		//int getFreeSpaceinMB();
+		static void eraseFlash();
+
+		static void transferSingleToFlash( const std::string& filepath );
+		//static void transferFolderToFlash( const std::string& filepath );
+		//std::vector< String > getAllSamplepacks();
+		//std::vector<String> getAllFiles();
+		//std::vector<String> getAllSamples( const String& samplepack );
+		//void initializeSamplepack( const String& samplepack );
+	};
+}// namespace lbs
