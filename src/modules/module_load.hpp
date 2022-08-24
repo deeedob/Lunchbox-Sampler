246caf5a00fbc7e81a09fc2f79e22b03fc322ea5
lWilbert2 <73698592+lWilbert2@users.noreply.github.com>
2022-08-24 16:14:30 +0200

    STARTING WITH UI, LOAD SAMPLES

--- src/modules/module_load.hpp
+++ src/modules/module_load.hpp
@@ -1,17 +1,30 @@
 #pragma once
 #include "abstract_module.hpp"
+#include <list>
 
 using namespace lbs;
 
 class ModuleLoad : public AbstractModule
 {
+	enum class STATE
+	{
+		SAMPLE,
+		SAMPLEPACK
+	};
 public:
 	ModuleLoad();
 	void enter( Graphics* g ) override;
 	void update( Graphics* g, Events::DIGITAL e ) override;
 	void exit() override;
 private:
+	void draw_load_menu1(Graphics* g);
+	void decrement_state();
+	void increment_state();
+	void loadSamples(Graphics* g);
+	void loadSamplePacks(Graphics* g);
+private:
+	std::vector<STATE> m_state;
+	std::vector<STATE>::iterator m_current;
 	Window m_topNav;
-	Window m_left;
-	Window m_right;
+	Window m_bottom;
 };
