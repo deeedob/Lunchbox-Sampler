de2bc7250f23138209b4af31c7100f6b9a70b35f
lWilbert2 <73698592+lWilbert2@users.noreply.github.com>
2022-08-26 15:17:31 +0200

    UPDATE MODULE_LOAD

--- src/modules/module_load.cpp
+++ src/modules/module_load.cpp
@@ -2,192 +2,220 @@
 
 using namespace lbs;
 
-ModuleLoad::ModuleLoad()
-	: AbstractModule( "Load Samples" )
-{
-	/*auto split_screens = WindowSize::createSplitScreen( UTIL::HORIZONTAL, 0.15f );
-	auto left_right = WindowSize::divideWindow( UTIL::VERTICAL, 0.3f, split_screens.second );
-	
-	m_topNav = Window( split_screens.first );
-	m_left = Window( left_right.first );
-	m_right = Window( left_right.second );*/
+ModuleLoad::ModuleLoad() : AbstractModule( "Load Samples" ) {
 	auto split_screens = WindowSize::createSplitScreen( UTIL::HORIZONTAL, 0.15 );
 	m_topNav = Window( split_screens.first );
 	m_bottom = Window( split_screens.second );
+	m_mainStates.push_back( STATE::LOAD_MODULE_MAIN );
+	m_mainStates.push_back( STATE::LOAD_MODULE_SUB_1 );
+	m_mainStates.push_back( STATE::LOAD_MODULE_SUB_2 );
+	m_mainStates.push_back( STATE::LOAD_MODULE_SUB_3 );
+	m_current_mainStates = m_mainStates.begin();
+
+	m_loadMenu.push_back( STATE::SAMPLE );
+	m_loadMenu.push_back( STATE::SAMPLEPACK );
+	m_current_subStates = m_loadMenu.begin();
+
+	m_AddOrRemove.push_back( STATE::ADD_SAMPLE_TO_FLASH );
+	m_AddOrRemove.push_back( STATE::REMOVE_SAMPLE_FROM_FLASH );
 }
-void ModuleLoad::increment_state()
-{
-	++m_current;
-	if( m_current == m_loadMenu.end())
-		m_current = m_loadMenu.begin();
-}
-void ModuleLoad::decrement_state()
-{
-	if( m_current == m_loadMenu.begin()) {
-		m_current = m_loadMenu.end();
-	}
-	m_current--;
-}
-void ModuleLoad::enter( Graphics* g )
-{
-	m_loadMenu.push_back(STATE::SAMPLE);
-	m_loadMenu.push_back(STATE::SAMPLEPACK);
-	m_current= m_loadMenu.begin();
-	draw_load_module( g );
-}
-void ModuleLoad::draw_load_module(Graphics* g)
-{
+void ModuleLoad::enter( Graphics* g ) { draw_load_module( g ); }
+void ModuleLoad::draw_load_module( Graphics* g ) {
 	m_topNav.fillScreen( 0xff );
 	m_topNav.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
-	m_topNav.printlnCentered( "LOAD SAMPLES");
-	
+	m_topNav.printlnCentered( "LOAD SAMPLES" );
+
 	m_bottom.fillScreen( 0x44 );
 	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
 	m_bottom.setTextPadding( { 4, 30 } );
-	if( m_loadMenu.begin()==m_current) {
+	if( m_loadMenu.begin() == m_current_subStates ) {
 		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter("SAMPLE", 0xff );
-	}
-	else {
+		m_bottom.printlnHoverCenter( "SAMPLE", 0xff );
+	} else {
 		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "SAMPLE");
+		m_bottom.printlnHCentered( "SAMPLE" );
 	}
-	if( m_loadMenu.begin()+1==m_current) {
+	if( m_loadMenu.begin() + 1 == m_current_subStates ) {
 		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter( "SAMPLEPACK", 0xff );;
-	}
-	else{
+		m_bottom.printlnHoverCenter( "SAMPLEPACK", 0xff );
+		;
+	} else {
 		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "SAMPLEPACK");
+		m_bottom.printlnHCentered( "SAMPLEPACK" );
 	}
 	g->drawWindow( m_topNav );
 	g->drawWindow( m_bottom );
 	g->display();
 }
-void ModuleLoad::update( Graphics* g, Events::DIGITAL e )
-{
-	if( e == Events::DIGITAL::BTN_RETURN && !m_loadMenu.empty()) {
-		Serial.println("MAIN MENU");
-		m_loadMenu.clear();
+void ModuleLoad::update( Graphics* g, Events::DIGITAL e ) {
+	//LOAD_MODULE_MAIN
+	//RETURN TO MAIN MENU
+	if( e == Events::DIGITAL::BTN_RETURN && m_current_mainStates == m_mainStates.begin() ) {
+		Serial.println( "MAIN MENU" );
 		exit();
 		return;
 	}
-	if( e == Events::DIGITAL::BTN_RETURN && m_loadMenu.empty()) {
-		m_loadMenu.push_back(STATE::SAMPLE);
-		m_loadMenu.push_back(STATE::SAMPLEPACK);
-		m_current= m_loadMenu.begin();
+	// CHOSE SAMPLE OR SAMPLEPACK WITH ROTARY
+	if( ( e == Events::DIGITAL::ROTARY_R || e == Events::DIGITAL::ROTARY_L ) &&
+	    m_current_mainStates == m_mainStates.begin() ) {
+		++m_current_subStates;
+		if( m_current_subStates == m_loadMenu.end() ) m_current_subStates = m_loadMenu.begin();
+	}
+	//GO TO LOAD_MODULE_SUB1 with LOAD SAMPLE
+	if( e == Events::DIGITAL::BTN_ENTER && m_current_mainStates == m_mainStates.begin() &&
+	    m_current_subStates == m_loadMenu.begin() ) {
+		Serial.println( "LOAD SAMPLE" );
+		m_current_mainStates++;
+		m_current_subStates = m_AddOrRemove.begin();
+		m_Sample_or_Samplepack = m_chooseFile.begin();
+		draw_load_module_sub1( g );
+		return;
+	}
+	//GO TO LOAD_MODULE_SUB1 with LOAD SAMPLEPACK
+	if( e == Events::DIGITAL::BTN_ENTER && m_current_mainStates == m_mainStates.begin() &&
+	    m_current_subStates == m_loadMenu.begin() + 1 ) {
+		m_current_mainStates++;
+		m_current_subStates = m_AddOrRemove.begin();
+		m_Sample_or_Samplepack = m_chooseFile.begin() + 1;
+		Serial.println( "LOAD SAMPLEPACK" );
+		draw_load_module_sub1( g );
+		return;
+	}
+
+	//LOAD_MODULE_SUB_1
+	// RETURN TO LOAD_MODULE_MAIN
+	if( e == Events::DIGITAL::BTN_RETURN && m_current_mainStates == m_mainStates.begin() + 1 ) {
+		m_current_mainStates--;
+		m_current_subStates = m_loadMenu.begin();
 		draw_load_module( g );
-		Serial.println("LOAD SAMPLE MAIN MENU");
 		return;
 	}
-	if( e == Events::DIGITAL::BTN_ENTER && m_current== m_loadMenu.begin()) {
-		Serial.println("LOAD SAMPLE");
-		draw_load_Samples(g);
+	//CHOSE ADD OR REMOVE SAMPLE WITH ROTARY
+	if( ( e == Events::DIGITAL::ROTARY_R || e == Events::DIGITAL::ROTARY_L ) &&
+	    m_current_mainStates == m_mainStates.begin() + 1 && m_Sample_or_Samplepack == m_chooseFile.begin() ) {
+		++m_current_subStates;
+		if( m_current_subStates == m_AddOrRemove.end() ) { m_current_subStates = m_AddOrRemove.begin(); }
+		draw_load_module_sub1( g );
+	}
+	if( e == Events::DIGITAL::BTN_ENTER && m_current_mainStates == m_mainStates.begin() + 1 &&
+	    m_Sample_or_Samplepack == m_chooseFile.begin() ) {
+		m_current_mainStates++;
+		Serial.println( "ADD SAMPLE/SAMPLEPACK" );
+		draw_load_module_sub2_ADD( g );
 		return;
 	}
-	if( e == Events::DIGITAL::BTN_ENTER && m_current== m_loadMenu.begin()+1) {
-		Serial.println("LOAD SAMPLEPACK");
-		draw_load_SamplePacks( g );
-		return;
-	}
-	if( e == Events::DIGITAL::BTN_ENTER && m_current==m_chooseFile.begin() && m_chooseNote.empty()) {
-		Serial.println("CHOOSE START CHOOSE_NOTE");
-		draw_choose_MidiNote( g );
-		return;
-	}
-	if( e == Events::DIGITAL::BTN_ENTER && m_current==m_chooseFile.begin()+1 && m_chooseNote.empty()) {
-		Serial.println("CHOOSE START CHOOSE_NOTE");
-		draw_choose_MidiNote( g );
-		return;
-	}
-	if( e == Events::DIGITAL::ROTARY_L &&!m_loadMenu.empty()){
-		decrement_state();
-	}
-	if( e == Events::DIGITAL::ROTARY_R &&!m_loadMenu.empty())
-	{increment_state();}
-	if( e == Events::DIGITAL::ROTARY_R &&!m_chooseNote.empty()) {
-		Key++;
-		if(Key==24)
+	//SUB 3   CHOOSE SAMPLE TO ADD/REMOVE WITH ROTARY
+	if( e == Events::DIGITAL::ROTARY_R &&m_current_mainStates == m_mainStates.begin() + 2 ) {
+		if(m_selected_sample<sizeof bsp_data_Flash/sizeof bsp_data_Flash[0]-1)
 		{
-			Key=0;
-			return;
+			Serial.println(sizeof bsp_data_Flash/sizeof bsp_data_Flash[0]);
+			++m_selected_sample;
+			if(m_selected_sample==m_first_visible_sample+7)
+			{
+				m_first_visible_sample++;
+			}
 		}
-		draw_choose_MidiNote( g );
-		return;
-	}
-	if( e == Events::DIGITAL::BTN_ENTER &&!m_chooseNote.empty()) {
-		Serial.println("uhu");
-		if(m_current==m_chooseFile.begin())
+		else
 		{
-			Serial.println("Back To Load Samples");
-			m_chooseNote.clear();
-			draw_load_Samples( g );
-			return;
+			m_first_visible_sample=0;
+			m_selected_sample=0;
 		}
-		if(m_current==m_chooseFile.begin()+1)
-		{
-			m_chooseNote.clear();
-			draw_load_SamplePacks( g );
-			return;
-		}
-		
+		draw_load_module_sub2_ADD( g );
 	}
-	if( e == Events::DIGITAL::ROTARY_L &&!m_chooseNote.empty()) {
-		Key--;
-		if(Key==0)
+	if( e == Events::DIGITAL::ROTARY_L &&m_current_mainStates == m_mainStates.begin() + 2 ) {
+		if(m_selected_sample==0)
 		{
-			Key=24;
-		}
-		Serial.println(Key);
-		draw_choose_MidiNote( g );
-	}
-	if(!m_loadMenu.empty()) draw_load_module( g );
-}
-void ModuleLoad::exit()
-{
-	notify( *this );
-}
-void ModuleLoad::draw_choose_MidiNote(Graphics* g){
-	m_chooseNote.push_back(STATE::CHOOSE_NOTE );
-	m_topNav.fillScreen(0xff);
-	m_topNav.setTextColor(0x44);
-	m_topNav.printlnCentered("MIDI");
-	m_bottom.fillScreen(0x00);
-	
-	int whiteKeys [14]={1,3,5,6,8,10,12,13,15,17,18,20,22,24};
-	int blackKeys [10]={2,4,7,9,11,14,16,19,21,23};
-	int wCount=0;
-	for(int i=8; i<=116; i=i+8)
-	{
-		if(whiteKeys[wCount]==Key)
-		{
-			m_bottom.drawRect(i,5,8,50,0xbb);
-			m_bottom.fillRect(i+1,6,6,48,0xbb);
+			m_selected_sample=sizeof bsp_data_Flash/sizeof bsp_data_Flash[0];
+			m_first_visible_sample=(sizeof bsp_data_Flash/sizeof bsp_data_Flash[0])-7;
 		}
 		else{
-			m_bottom.drawRect(i,5,8,50,0x44);
-			m_bottom.fillRect(i+1,6,6,48,0xff);
+			if(m_selected_sample==m_first_visible_sample)
+			{
+				m_first_visible_sample--;
+			}
+		}
+		m_selected_sample--;
+		draw_load_module_sub2_ADD( g );
+	}
+	if( e == Events::DIGITAL::BTN_RETURN && m_current_mainStates == m_mainStates.begin() + 2 ) {
+		m_current_mainStates--;
+		m_current_subStates = m_Sample_or_Samplepack;
+		draw_load_module_sub1( g );
+		return;
+	}
+	if( e == Events::DIGITAL::BTN_ENTER && m_current_mainStates == m_mainStates.begin() + 2)
+	{
+		m_current_mainStates++;
+		Serial.println( "CHOOSE START CHOOSE_NOTE" );
+		draw_load_module_sub3( g );
+		return;
+	}
+
+	//CHOSE START NOTE POSITION FOR SAMPLEPACK
+	if( e == Events::DIGITAL::BTN_ENTER && m_current_mainStates == m_mainStates.begin() + 3 &&
+	    m_Sample_or_Samplepack == m_chooseFile.begin() + 1 ) {
+		m_current_mainStates++;
+		Serial.println( "CHOOSE START CHOOSE_NOTE" );
+		draw_load_module_sub3( g );
+		return;
+	}
+
+	//LOAD_MODULE_SUB_3:
+	//CHOSE NOTE POSITION FOR SAMPLE
+	//MOVE KEY WITH ROTARY
+	if( e == Events::DIGITAL::ROTARY_R && m_current_mainStates == m_mainStates.begin() + 3 ) {
+		m_key--;
+		if( m_key == 0 ) {
+			m_key = 24;
+			return;
+		}
+		draw_load_module_sub3( g );
+		return;
+	}
+	if( e == Events::DIGITAL::ROTARY_L && m_current_mainStates == m_mainStates.begin() + 3 ) {
+		m_key++;
+		if( m_key == 24 ) { m_key = 0; }
+		draw_load_module_sub3( g );
+	}
+	if( e == Events::DIGITAL::BTN_ENTER && m_current_mainStates == m_mainStates.begin() + 3 ) {
+		m_current_mainStates--;
+		draw_load_module_sub1( g );
+		return;
+	}
+	if( m_current_mainStates == m_mainStates.begin() ) draw_load_module( g );
+}
+void ModuleLoad::exit() { notify( *this ); }
+void ModuleLoad::draw_load_module_sub3( Graphics* g ) {
+	m_topNav.fillScreen( 0xff );
+	m_topNav.setTextColor( 0x44 );
+	m_topNav.printlnCentered( "MIDI" );
+	m_bottom.fillScreen( 0x00 );
+
+	int whiteKeys[ 14 ] = { 1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20, 22, 24 };
+	int blackKeys[ 10 ] = { 2, 4, 7, 9, 11, 14, 16, 19, 21, 23 };
+	int wCount = 0;
+	for( int i = 8; i <= 116; i = i + 8 ) {
+		if( whiteKeys[ wCount ] == m_key ) {
+			m_bottom.drawRect( i, 5, 8, 50, 0xbb );
+			m_bottom.fillRect( i + 1, 6, 6, 48, 0xbb );
+		} else {
+			m_bottom.drawRect( i, 5, 8, 50, 0x44 );
+			m_bottom.fillRect( i + 1, 6, 6, 48, 0xff );
 		}
 		wCount++;
 	}
-	int count=0;
-	int bcount=0;
-	for(int i=8 ; i<116; i=i+8)
-	{
-		if(count!=2 && count!=6 && count!=9 &&count!=13)
-		{
-			if(blackKeys[bcount]==Key)
-			{
-				m_bottom.drawRect(i+5,5,6,30,0x44);
-				m_bottom.fillRect(i+6,6,4,28,0xbb);
-			}
-			else
-			{
-				m_bottom.drawRect(i+5,5,6,30,0x44);
-				m_bottom.fillRect(i+6,6,4,28,0x11);
+	int count = 0;
+	int bcount = 0;
+	for( int i = 8; i < 116; i = i + 8 ) {
+		if( count != 2 && count != 6 && count != 9 && count != 13 ) {
+			if( blackKeys[ bcount ] == m_key ) {
+				m_bottom.drawRect( i + 5, 5, 6, 30, 0x44 );
+				m_bottom.fillRect( i + 6, 6, 4, 28, 0xbb );
+			} else {
+				m_bottom.drawRect( i + 5, 5, 6, 30, 0x44 );
+				m_bottom.fillRect( i + 6, 6, 4, 28, 0x11 );
 			}
 			bcount++;
-	
 		}
 		count++;
 	}
@@ -195,50 +223,70 @@ void ModuleLoad::draw_choose_MidiNote(Graphics* g){
 	g->drawWindow( m_bottom );
 	g->display();
 }
-void ModuleLoad::draw_load_SamplePacks(Graphics* g) {
-	m_loadMenu.clear();
-	m_chooseFile.clear();
-	m_chooseNote.clear();
-	m_chooseFile.push_back(STATE::CHOOSE_SAMPLE );
-	m_chooseFile.push_back(STATE::CHOOSE_SAMPLEPACK );
-	m_current=m_chooseFile.begin()+1;
+void ModuleLoad::draw_load_module_sub1( Graphics* g ) {
 	g->clearDisplay();
 	m_topNav.fillScreen( 0xff );
 	m_topNav.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
-	m_topNav.printlnCentered( "LOAD SAMPLEPACKS");
-	
+	m_topNav.printlnCentered( "LOAD SAMPLE" );
+
+	auto split_screens2 = WindowSize::createSplitScreen( UTIL::HORIZONTAL, 0.5 );
+	Window m_Add = Window( split_screens2.first );
+	Window m_Remove = Window( split_screens2.second );
+
 	m_bottom.fillScreen( 0x44 );
-	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
-	m_bottom.setTextPadding( { 4, 4 } );
-	m_bottom.setTextColor(0xFF);
-	m_bottom.fillRect(8,8,110,45,0xFF);
-	m_bottom.fillRect(8,55,110,45,0xFF);
-	
+	//Mit Rotary eine der Boxen auswählen
+	if( m_current_subStates == m_AddOrRemove.begin() ) {
+		m_Add.fillScreen( 0XFF );
+		m_Add.drawWindowBorder( { 4, 4 }, 2, 0x00, 2 );
+		m_Add.setTextPadding( { 4, 4 } );
+		m_Add.setTextColor( 0x00 );
+		m_Add.printlnHCentered( "ADD TO FLASH" );
+	} else {
+		m_Add.fillScreen( 0X00 );
+		m_Add.drawWindowBorder( { 4, 4 }, 2, 0xFF, 2 );
+		m_Add.setTextPadding( { 4, 4 } );
+		m_Add.setTextColor( 0xFF );
+		m_Add.printlnHCentered( "ADD TO FLASH" );
+	}
+	if( m_current_subStates == m_AddOrRemove.begin() + 1 ) {
+		m_Remove.fillScreen( 0XFF );
+		m_Remove.drawWindowBorder( { 4, 4 }, 2, 0x00, 2 );
+		m_Remove.setTextPadding( { 8, 4 } );
+		m_Remove.setTextColor( 0x00 );
+		m_Remove.printlnHCentered( "REMOVE FROM FLASH" );
+	} else {
+		m_Remove.fillScreen( 0X00 );
+		m_Remove.drawWindowBorder( { 4, 4 }, 2, 0xFF, 2 );
+		m_Remove.setTextPadding( { 4, 4 } );
+		m_Remove.setTextColor( 0xFF );
+		m_Remove.printlnHCentered( "REMOVE FROM FLASH");
+	}
+
 	g->drawWindow( m_topNav );
-	g->drawWindow( m_bottom );
+	g->drawWindow( m_Add );
+	g->drawWindow( m_Remove );
 	g->display();
-	
 }
-void ModuleLoad::draw_load_Samples(Graphics* g) {
-	m_loadMenu.clear();
-	m_chooseFile.clear();
-	m_chooseNote.clear();
-	m_chooseFile.push_back(STATE::CHOOSE_SAMPLE );
-	m_chooseFile.push_back(STATE::CHOOSE_SAMPLEPACK );
-	m_current=m_chooseFile.begin();
-	
+void ModuleLoad::draw_load_module_sub2_ADD( Graphics* g ) {
 	g->clearDisplay();
+
 	m_topNav.fillScreen( 0xff );
 	m_topNav.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
-	m_topNav.printlnCentered( "LOAD SAMPLE");
-	
+	m_topNav.printlnCentered( "ADD SAMPLE TO FLASH" );
 	m_bottom.fillScreen( 0x44 );
 	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
 	m_bottom.setTextPadding( { 4, 4 } );
-	m_bottom.setTextColor(0xFF);
-	m_bottom.fillRect(8,8,110,45,0xFF);
-	m_bottom.fillRect(8,55,110,45,0xFF);
-	
+
+	for( int i = m_first_visible_sample; i < m_first_visible_sample + 7; i++ ) {
+		if( i == m_selected_sample ) {
+			m_bottom.setTextColor( 0x00 );
+			m_bottom.printlnHoverCenter( bsp_data_Flash[ i ].c_str(), 0xff );
+		} else {
+			m_bottom.setTextColor( 0xff );
+			m_bottom.printlnHCentered( bsp_data_Flash[ i ].c_str() );
+		}
+	}
+
 	g->drawWindow( m_topNav );
 	g->drawWindow( m_bottom );
 	g->display();
--- src/modules/module_load.hpp
+++ src/modules/module_load.hpp
@@ -2,17 +2,27 @@
 #include "abstract_module.hpp"
 #include <list>
 
+
 using namespace lbs;
 
 class ModuleLoad : public AbstractModule
 {
 	enum class STATE
 	{
+		
+		//MAINSTATES: SEITEN
+		LOAD_MODULE_MAIN,     //SAMPLE OR SAMPLEPACK?
+		LOAD_MODULE_SUB_1,    //ADD FILE TO FLASH OR REMOVE FILE FROM FLASH
+		LOAD_MODULE_SUB_2,    //CHOOSE FILE TO ADD/REMOVE
+		LOAD_MODULE_SUB_3,     //CHOOSE NOTE FOR SAMPLE/ STARTNOTE FOR SAMPLEPACK
+		
+		//SUBMENU STATES
 		SAMPLE,
 		SAMPLEPACK,
+		ADD_SAMPLE_TO_FLASH,
+		REMOVE_SAMPLE_FROM_FLASH,
 		CHOOSE_SAMPLEPACK,
 		CHOOSE_SAMPLE,
-		CHOOSE_NOTE,
 	};
 public:
 	ModuleLoad();
@@ -21,17 +31,23 @@ public:
 	void exit() override;
 private:
 	void draw_load_module(Graphics* g);
-	void decrement_state();
-	void increment_state();
-	void draw_load_Samples(Graphics* g);
-	void draw_load_SamplePacks(Graphics* g);
-	void draw_choose_MidiNote(Graphics* g);
+	void draw_load_module_sub1(Graphics* g);
+	void draw_load_module_sub2_ADD(Graphics* g);
+	void draw_load_module_sub3(Graphics* g);
 private:
+	std::vector <STATE> m_mainStates;
 	std::vector<STATE> m_loadMenu;
 	std::vector<STATE> m_chooseFile;
-	std::vector<STATE> m_chooseNote;
-	std::vector<STATE>::iterator m_current;
+	std::vector<STATE> m_AddOrRemove;
+	std::vector<STATE>::iterator m_current_mainStates;
+	std::vector<STATE>::iterator m_current_subStates;
+	bool isSample= true;
+	bool isAdd=true;
+	std::string bsp_data_Flash[12]={"Sample1","Sample2", "Sample3", "Sample4", "Sample5", "Sample6", "Sample7", "Sample8", "Sample9", "Sample10", "Sample11", "Sample12"};
 	Window m_topNav;
 	Window m_bottom;
-	int Key=1;
+	
+	int m_key =1;
+	int m_selected_sample =0;
+	int m_first_visible_sample =0;
 };
