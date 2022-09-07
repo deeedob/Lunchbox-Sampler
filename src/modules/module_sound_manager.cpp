577b2eaa02af0f3a51d662c8da58e5c91dd9e5d8
lWilbert2 <73698592+lWilbert2@users.noreply.github.com>
2022-09-07 16:58:27 +0200

    UPDATE MODULE LOAD AND SOUND MANAGER, AUDIO, STILL MESSY WITH OLD CLASSES; NEEDS CLEAN UP

--- src/modules/module_sound_manager.cpp
+++ src/modules/module_sound_manager.cpp
@@ -8,53 +8,36 @@ ModuleSoundManager::ModuleSoundManager() : AbstractModule( "Sound Manager" ) {
 	auto split_screens = WindowSize::createSplitScreen( UTIL::HORIZONTAL, 0.15 );
 	m_top = Window( split_screens.first );
 	m_bottom = Window( split_screens.second );
+	auto split_screens2 = WindowSize::createSplitScreen( UTIL::VERTICAL, 0.5 );
+	m_left = Window( split_screens2.first );
+	m_right = Window( split_screens2.second );
 	m_mainStates.push_back( STATE::SOUND_MANAGER_MODULE_MAIN );
 	m_mainStates.push_back( STATE::SOUND_MANAGER_MODULE_SUB_1 );
 	m_mainStates.push_back( STATE::SOUND_MANAGER_MODULE_SUB_2 );
 	m_mainStates.push_back( STATE::SOUND_MANAGER_MODULE_SUB_3 );
 
-	m_subStates.push_back( STATE::VOLUME );
-	m_subStates.push_back( STATE::FILTER1 );
-	m_subStates.push_back( STATE::FILTER2 );
-	m_subStates.push_back( STATE::REVERSE );
-	m_subStates.push_back( STATE::CUT );
+	m_subStates.push_back( STATE::REMOVE_SAMPLES );
+	m_subStates.push_back( STATE::CHANGE_ROUTING );
+
+	bsp_data_Flash.push_back( { "0.wav", 48 } );
+	bsp_data_Flash.push_back( { "1.wav", 49 } );
+	bsp_data_Flash.push_back( { "2.wav", 50 } );
+	bsp_data_Flash.push_back( { "3.wav", 51 } );
+	bsp_data_Flash.push_back( { "4.wav", 52 } );
+	bsp_data_Flash.push_back( { "5.wav", 53 } );
+	bsp_data_Flash.push_back( { "6.wav", 54} );
+	bsp_data_Flash.push_back( { "7.wav", 55 } );
+	bsp_data_Flash.push_back( { "8.wav", 56 } );
+	bsp_data_Flash.push_back( { "9.wav", 57 } );
+	bsp_data_Flash.push_back( { "10.wav", 58 } );
+	bsp_data_Flash.push_back( { "11.wav", 59 } );
+	bsp_data_Flash.push_back( { "12.wav", 60 } );
 }
 
 void ModuleSoundManager::enter( Graphics* g ) {
+	m_currentSubState = m_subStates.begin();
 	m_currentMainState = m_mainStates.begin();
 	draw_sound_manager_module_main( g );
-}
-void ModuleSoundManager::draw_sound_manager_module_main( Graphics* g ) {
-	m_top.fillScreen( 0xff );
-	m_top.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
-	m_top.printlnCentered( "Choose Samplepack" );
-	m_bottom.fillScreen( 0x44 );
-	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
-	m_bottom.setTextPadding( { 4, 4 } );
-	if( sizeof bsp_data_Flash2 / sizeof bsp_data_Flash2[ 0 ] >= 7 ) {
-		for( int i = m_first_visible_sample; i < m_first_visible_sample + 7; i++ ) {
-			if( i == m_selected_pack ) {
-				m_bottom.setTextColor( 0x00 );
-				m_bottom.printlnHoverCenter( bsp_data_Flash2[ i ][ 0 ].c_str(), 0xff );
-			} else {
-				m_bottom.setTextColor( 0xff );
-				m_bottom.printlnHCentered( bsp_data_Flash2[ i ][ 0 ].c_str() );
-			}
-		}
-	} else {
-		for( int i = 0; i < sizeof bsp_data_Flash2 / sizeof bsp_data_Flash2[ 0 ]; i++ ) {
-			if( i == m_selected_pack ) {
-				m_bottom.setTextColor( 0x00 );
-				m_bottom.printlnHoverCenter( bsp_data_Flash2[ i ][ 0 ].c_str(), 0xff );
-			} else {
-				m_bottom.setTextColor( 0xff );
-				m_bottom.printlnHCentered( bsp_data_Flash2[ i ][ 0 ].c_str() );
-			}
-		}
-	}
-	g->drawWindow( m_top );
-	g->drawWindow( m_bottom );
-	g->display();
 }
 void ModuleSoundManager::draw_sound_manager_module_sub1( Graphics* g ) {
 	g->clearDisplay();
@@ -63,122 +46,231 @@ void ModuleSoundManager::draw_sound_manager_module_sub1( Graphics* g ) {
 	m_top.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
 	m_top.printlnCentered( "Choose Sample" );
 
-	m_bottom.fillScreen( 0x44 );
-	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
-	m_bottom.setTextPadding( { 4, 4 } );
+	m_left.fillScreen( 0x44 );
+	m_left.drawWindowBorder( { 2, 2 }, 2, 0xbb, 1 );
+	m_left.setTextPadding( { 2, 20 } );
+	m_right.fillScreen( 0x44 );
+	m_right.drawWindowBorder( { 2, 2 }, 2, 0xbb, 1 );
+	m_right.setTextPadding( { 2, 20 } );
 
-	if( sizeof bsp_data_Flash2[ m_selected_pack ] / sizeof bsp_data_Flash2[ m_selected_pack ][ 0 ] >= 7 ) {
-		for( int i = m_first_visible_sample; i < m_first_visible_sample + 7; i++ ) {
+	if( sizeof bsp_data_Flash >= 8 ) {
+		for( int i = m_first_visible_sample; i < m_first_visible_sample + 8; i++ ) {
 			if( i == m_selected_sample ) {
-				m_bottom.setTextColor( 0x00 );
-				m_bottom.printlnHoverCenter( bsp_data_Flash2[ m_selected_pack ][ i ].c_str(), 0xff );
+				m_left.setTextColor( 0x00 );
+				m_left.printlnHoverCenter( bsp_data_Flash[ i ].name.c_str(), 0xff );
+				m_right.setTextColor( 0x00 );
+				m_right.printlnHoverCenter( MidiNotes[bsp_data_Flash[ i ].midiNote].c_str(), 0xff );
 			} else {
-				m_bottom.setTextColor( 0xff );
-				m_bottom.printlnHCentered( bsp_data_Flash2[ m_selected_pack ][ i ].c_str() );
+				m_left.setTextColor( 0xff );
+				m_left.printlnHCentered( bsp_data_Flash[ i ].name.c_str() );
+				m_right.setTextColor( 0xff );
+				m_right.printlnHCentered( MidiNotes[bsp_data_Flash[ i ].midiNote].c_str());
 			}
 		}
 	} else {
-		for( int i = 1; i < sizeof bsp_data_Flash2[ m_selected_pack ] / sizeof bsp_data_Flash2[ m_selected_pack ][ 0 ];
-		     i++ ) {
+		for( int i = 1; i < sizeof bsp_data_Flash; i++ ) {
 			if( i == m_selected_sample ) {
-				m_bottom.setTextColor( 0x00 );
-				m_bottom.printlnHoverCenter( bsp_data_Flash2[ m_selected_pack ][ i ].c_str(), 0xff );
+				m_left.setTextColor( 0x00 );
+				m_left.printlnHoverCenter( bsp_data_Flash[ i ].name.c_str(), 0xff );
+				m_right.setTextColor( 0x00 );
+				m_right.printlnHoverCenter( MidiNotes[bsp_data_Flash[ i ].midiNote].c_str(), 0xff );
 			} else {
-				m_bottom.setTextColor( 0xff );
-				m_bottom.printlnHCentered( bsp_data_Flash2[ m_selected_pack ][ i ].c_str() );
+				m_left.setTextColor( 0xff );
+				m_left.printlnHCentered( bsp_data_Flash[ i ].name.c_str() );
+				m_right.setTextColor( 0xff );
+				m_right.printlnHCentered( MidiNotes[bsp_data_Flash[ i ].midiNote].c_str() );
 			}
 		}
 	}
+	g->drawWindow( m_left );
+	g->drawWindow( m_right );
 	g->drawWindow( m_top );
-	g->drawWindow( m_bottom );
 	g->display();
 }
-void ModuleSoundManager::draw_sound_manager_module_sub2( Graphics* g ) {
+void ModuleSoundManager::draw_sound_manager_module_main( Graphics* g ) {
 	m_top.fillScreen( 0xff );
 	m_top.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
-	m_top.printlnCentered( "CHOOSE EFFECT" );
+	m_top.printlnCentered( "SOUND MANAGER" );
 
 	m_bottom.fillScreen( 0x44 );
 	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
-	m_bottom.setTextPadding( { 4, 4 } );
+	m_bottom.setTextPadding( { 4, 32 } );
 	if( m_subStates.begin() == m_currentSubState ) {
 		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter( "CHANGE VOLUME", 0xff );
+		m_bottom.printlnHoverCenter( "REMOVE SAMPLEPACK", 0xff );
 	} else {
 		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "CHANGE VOLUME" );
+		m_bottom.printlnHCentered( "REMOVE SAMPLEPACK" );
 	}
 	if( m_subStates.begin() + 1 == m_currentSubState ) {
 		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter( "FILTER1", 0xff );
+		m_bottom.printlnHoverCenter( "CHANGE ROUTING", 0xff );
 		;
 	} else {
 		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "FILTER1" );
-	}
-	if( m_subStates.begin() + 2 == m_currentSubState ) {
-		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter( "FILTER2", 0xff );
-		;
-	} else {
-		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "FILTER2" );
-	}
-	if( m_subStates.begin() + 3 == m_currentSubState ) {
-		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter( "REVERSE", 0xff );
-		;
-	} else {
-		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "REVERSE" );
-	}
-	if( m_subStates.begin() + 4 == m_currentSubState ) {
-		m_bottom.setTextColor( 0x00 );
-		m_bottom.printlnHoverCenter( "CUT", 0xff );
-		;
-	} else {
-		m_bottom.setTextColor( 0xff );
-		m_bottom.printlnHCentered( "CUT" );
-	}
-	g->drawWindow( m_top );
-	g->drawWindow( m_bottom );
-	g->display();
-}
-void ModuleSoundManager::draw_sound_manager_module_sub3( Graphics* g ) {
-	g->clearDisplay();
-	m_top.fillScreen( 0xff );
-	m_top.drawWindowBorder( { 2, 2 }, 0, 0x00, 1 );
-	m_bottom.fillScreen( 0x44 );
-	m_bottom.drawWindowBorder( { 4, 4 }, 2, 0xbb, 2 );
-	m_bottom.setTextPadding( { 4, 4 } );
-	if( m_currentSubState == m_subStates.begin() ) {
-		m_top.printlnCentered( "CHANGE VOLUME" );
-		m_bottom.drawRect( 12, 12, 8, 86, 0xFF );
-		m_bottom.drawRect( 24, 12, 90, 86, 0xFF );
-		m_lastValue = analogRead( 14 );
-		float p = m_lastValue / 1023 * 100;
-		int x = 86 * p / 100;
-		Serial.println( m_lastValue );
-		Serial.println( x );
-		m_bottom.fillRect( 12, 12 + ( 86 - x ), 6, x, 0xFF );
-	} else if( m_currentSubState == m_subStates.begin() + 1 ) {
-		m_top.printlnCentered( "FILTER 1" );
-	} else if( m_currentSubState == m_subStates.begin() + 2 ) {
-		m_top.printlnCentered( "FILTER 2" );
-	} else if( m_currentSubState == m_subStates.begin() + 3 ) {
-		m_top.printlnCentered( "REVERSE SAMPLE" );
-	}
-	else if( m_currentSubState == m_subStates.begin() + 4 ) {
-		m_top.printlnCentered( "CUT" );
-		
+		m_bottom.printlnHCentered( "CHANGE ROUTING" );
 	}
 	g->drawWindow( m_top );
 	g->drawWindow( m_bottom );
 	g->display();
 }
 void ModuleSoundManager::update( Graphics* g, Events::DIGITAL e ) {
-	//MAIN --> CHOOSE SAMPLEPACK
+	//MAIN -->SOUND MANAGER
 	if( m_currentMainState == m_mainStates.begin() ) {
+		if( e == Events::DIGITAL::ROTARY_R ) {
+			m_currentSubState++;
+			if( m_currentSubState == m_subStates.end() ) { m_currentSubState = m_subStates.begin(); }
+			draw_sound_manager_module_main( g );
+			return;
+		}
+		if( e == Events::DIGITAL::ROTARY_L ) {
+			if( m_currentSubState == m_subStates.begin() ) { m_currentSubState = m_subStates.end(); }
+			m_currentSubState--;
+			draw_sound_manager_module_main( g );
+			return;
+		}
+		if( e == Events::DIGITAL::BTN_ENTER ) {
+			if( m_currentSubState == m_subStates.begin() ) {
+				m_currentMainState--;
+				draw_load_module_loadbar( g );
+				return;
+				//lade
+			}
+			if( m_currentSubState == m_subStates.begin() + 1 ) {
+				m_currentMainState++;
+				draw_sound_manager_module_sub1( g );
+				return;
+			}
+		}
+		if( e == Events::DIGITAL::BTN_RETURN ) {
+			exit();
+			return;
+		}
+	}
+	if( m_currentMainState == m_mainStates.begin() + 1 ) {
+		if( e == Events::DIGITAL::ROTARY_R ) {
+			if( m_selected_sample < sizeof bsp_data_Flash ) {
+				++m_selected_sample;
+				if( sizeof bsp_data_Flash >= 8 ) {
+					if( m_selected_sample == m_first_visible_sample + 8 ) { m_first_visible_sample++; }
+				}
+			} else {
+				m_first_visible_sample = 0;
+				m_selected_sample = 0;
+			}
+			draw_sound_manager_module_sub1( g );
+			return;
+		}
+		if( e == Events::DIGITAL::ROTARY_L ) {
+			if( m_selected_sample == 1 ) {
+				m_selected_sample = sizeof bsp_data_Flash;
+				m_first_visible_sample = sizeof bsp_data_Flash - 8;
+			} else {
+				if( m_selected_sample == m_first_visible_sample ) { m_first_visible_sample--; }
+			}
+			m_selected_sample--;
+			draw_sound_manager_module_sub1( g );
+			return;
+		}
+		if( e == Events::DIGITAL::BTN_RETURN ) {
+			m_currentMainState--;
+			draw_sound_manager_module_main( g );
+			return;
+		}
+		if( e == Events::DIGITAL::BTN_ENTER ) {
+			m_currentMainState++;
+			m_key=bsp_data_Flash[m_selected_sample].midiNote;
+			m_note=m_key;
+			draw_key_mapping(g);
+			return;
+		}
+	}
+	if( m_currentMainState == m_mainStates.begin() + 2 ) {
+		if( e == Events::DIGITAL::ROTARY_R ) {
+			m_key--;
+			m_note--;
+			if( m_key == 0 ) {
+				m_key = 24;
+				return;
+			}
+			draw_key_mapping( g );
+			return;
+		}
+		if( e == Events::DIGITAL::ROTARY_L ) {
+			m_key++;
+			m_note++;
+			if( m_key == 24 ) { m_key = 0; }
+			draw_key_mapping( g );
+			return;
+		}
+	}
+}
+void ModuleSoundManager::draw_load_module_loadbar( Graphics* g ) {
+	m_bottom.fillScreen( 0x00 );
+	m_bottom.drawRect( 12, 50, 100, 8, 0xFF );
+	for( int i = 0; i < 20; i++ ) {
+		m_bottom.fillRect( 12, 50, i * 5, 8, 0xFF );
+		m_bottom.fillRect( 20, 60, 100, 20, 0x00 );
+		m_bottom.setTextColor( 0xff );
+		m_bottom.setCursor( 58, 70 );
+		m_bottom.print( i * 5 );
+		m_bottom.print( "%" );
+		g->drawWindow( m_bottom );
+		g->display();
+		delay( 20 );
+	}
+	exit();
+}
+void ModuleSoundManager::draw_key_mapping( Graphics* g ) {
+	m_top.fillScreen( 0xff );
+	m_top.setTextColor( 0x44 );
+	m_top.printlnCentered( "MIDI" );
+	m_bottom.fillScreen( 0x00 );
+	if(m_key>=24) {
+		for( int i = 1; i < 4; i++ ) {
+			if( m_key >= i * 24 && m_key < (i+1) * 24 -1 ) {
+				m_key = m_key - i * 24;
+				break;
+			}
+		}
+	}
+	int whiteKeys[ 14 ] = { 1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20, 22, 24 };
+	int blackKeys[ 10 ] = { 2, 4, 7, 9, 11, 14, 16, 19, 21, 23 };
+	int wCount = 0;
+	for( int i = 8; i <= 116; i = i + 8 ) {
+		if( whiteKeys[ wCount ] == m_key ) {
+			m_bottom.drawRect( i, 5, 8, 50, 0xbb );
+			m_bottom.fillRect( i + 1, 6, 6, 48, 0xbb );
+		} else {
+			m_bottom.drawRect( i, 5, 8, 50, 0x44 );
+			m_bottom.fillRect( i + 1, 6, 6, 48, 0xff );
+		}
+		wCount++;
+	}
+	int count = 0;
+	int bcount = 0;
+	for( int i = 8; i < 116; i = i + 8 ) {
+		if( count != 2 && count != 6 && count != 9 && count != 13 ) {
+			if( blackKeys[ bcount ] == m_key ) {
+				m_bottom.drawRect( i + 5, 5, 6, 30, 0x44 );
+				m_bottom.fillRect( i + 6, 6, 4, 28, 0xbb );
+			} else {
+				m_bottom.drawRect( i + 5, 5, 6, 30, 0x44 );
+				m_bottom.fillRect( i + 6, 6, 4, 28, 0x11 );
+			}
+			bcount++;
+		}
+		count++;
+	}
+	m_bottom.setCursor(62,70);
+	m_bottom.setTextColor(0xff);
+	m_bottom.println(MidiNotes[m_note].c_str());
+	g->drawWindow( m_top );
+	g->drawWindow( m_bottom );
+	g->display();
+}
+
+/*if( m_currentMainState == m_mainStates.begin()+2 ) {
 		if( e == Events::DIGITAL::ROTARY_L ) {
 			if( m_selected_pack == 0 ) {
 				m_selected_pack = sizeof bsp_data_Flash2 / sizeof bsp_data_Flash2[ 0 ];
@@ -257,32 +349,7 @@ void ModuleSoundManager::update( Graphics* g, Events::DIGITAL e ) {
 			return;
 		}
 	}
-	//SUB2 --> CHOOSE EFFECT
-	if( m_currentMainState == m_mainStates.begin() + 2 ) {
-		if( e == Events::DIGITAL::ROTARY_R ) {
-			m_currentSubState++;
-			if( m_currentSubState == m_subStates.end() ) { m_currentSubState = m_subStates.begin(); }
-			draw_sound_manager_module_sub2( g );
-			return;
-		}
-		if( e == Events::DIGITAL::ROTARY_L ) {
-			if( m_currentSubState == m_subStates.begin() ) { m_currentSubState = m_subStates.end(); }
-			m_currentSubState--;
-			draw_sound_manager_module_sub2( g );
-			return;
-		}
-		if( e == Events::DIGITAL::BTN_ENTER ) {
-			m_currentMainState++;
-			draw_sound_manager_module_sub3( g );
-			return;
-		}
-		if( e == Events::DIGITAL::BTN_RETURN ) {
-			m_currentSubState = m_subStates.begin();
-			m_currentMainState--;
-			draw_sound_manager_module_sub1( g );
-			return;
-		}
-	}
+	
 	//SUB3 --> EFFECTS
 	if( m_currentMainState == m_mainStates.begin() + 3 ) {
 		if( e == Events::DIGITAL::BTN_RETURN ) {
@@ -296,7 +363,5 @@ void ModuleSoundManager::update( Graphics* g, Events::DIGITAL e ) {
 			draw_sound_manager_module_sub3( g );
 			return;
 		}
-	}
-}
-
+	}*/
 void ModuleSoundManager::exit() { notify( *this ); }
--- src/modules/module_sound_manager.hpp
+++ src/modules/module_sound_manager.hpp
@@ -5,6 +5,11 @@ using namespace lbs;
 
 class ModuleSoundManager : public AbstractModule
 {
+	struct file
+	{
+		String name;
+		int midiNote;
+	};
 	enum class STATE
 	{
 		//MainStates
@@ -14,14 +19,8 @@ class ModuleSoundManager : public AbstractModule
 		SOUND_MANAGER_MODULE_SUB_3,    //DETAIL EFFECT
 		
 		//SubStates
-		REVERSE,
-		FILTER1,
-		FILTER2,
-		VOLUME,
-		CUT,
-		
-		
-		//...
+		CHANGE_ROUTING,
+		REMOVE_SAMPLES,
 	};
 public:
 	ModuleSoundManager();
@@ -33,6 +32,8 @@ private:
 	void increment_state();
 	void draw_sound_manager_module_main(Graphics* g);
 	void draw_sound_manager_module_sub1(Graphics* g);
+	void draw_load_module_loadbar(Graphics* g);
+	void draw_key_mapping( Graphics*g);
 	void draw_sound_manager_module_sub2(Graphics* g);
 	void draw_sound_manager_module_sub3(Graphics* g);
 	
@@ -41,12 +42,28 @@ private:
 	std::vector<STATE> m_subStates;
 	std::vector<STATE>::iterator m_currentMainState;
 	std::vector<STATE>::iterator m_currentSubState;
-	std::string bsp_data_Flash[12]={"Sample1","Sample2", "Sample3", "Sample4", "Sample5", "Sample6", "Sample7", "Sample8", "Sample9", "Sample10", "Sample11", "Sample12"};
-	std::string bsp_data_Flash2[6][12]={{"SamplePack1","Sample1","Sample2", "Sample3", "Sample4", "Sample5", "Sample6", "Sample7", "Sample8", "Sample9", "Sample10", "Sample11"},{"SamplePack2"}, {"SamplePack3"}, {"SamplePack4"}, {"SamplePack5"},{"SamplePack6"}};
+	std::vector <file> bsp_data_Flash;
+	std::string MidiNotes [128]{
+	    "C-1","C#-1", "D-1","D#-1","E-1","F-1","F#-1","G-1","G#-1","A-1","A#-1","B-1",
+	    "C0","C#0", "D0","D#0","E0","F0","F#0","G0","G#0","A0","A#0","B0",
+	    "C1","C#1", "D1","D#1","E1","F1","F#1","G1","G#1","A1","A#1","B1",
+	    "C2","C#2", "D2","D#2","E2","F2","F#2","G2","G#2","A2","A#2","B2",
+	    "C3","C#3", "D3","D#3","E3","F3","F#3","G3","G#3","A3","A#3","B3",
+	    "C4","C#4", "D4","D#4","E4","F4","F#4","G4","G#4","A4","A#4","B4",
+	    "C5","C#5", "D5","D#5","E5","F5","F#5","G5","G#5","A5","A#5","B5",
+	    "C6","C#6", "D6","D#6","E6","F6","F#6","G6","G#6","A6","A#6","B6",
+	    "C7","C#7", "D7","D#7","E7","F7","F#7","G7","G#7","A7","A#7","B7",
+	    "C8","C#8", "D8","D#8","E8","F8","F#8","G8","G#8","A8","A#8","B8",
+	    "C9","C#9", "D9","D#9","E9","F9","F#9", "G9"
+	};
 	float m_lastValue;
 	Window m_bottom;
 	Window m_top;
+	Window m_left;
+	Window m_right;
 	
+	int m_key=0;
+	int m_note;
 	int m_selected_sample =0;
 	int m_selected_pack =0;
 	int m_first_visible_sample =0;
