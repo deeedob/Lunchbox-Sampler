46a5d153e487326a57d06a1cc408ed6672389ace
Lucas Haupt <lucas.haupt@smail.th-koeln.de>
2022-09-14 01:02:58 +0200

    prepare class as todo list for integrating every missing function one by one

--- lib/lbs-memory/main_memory.hpp
+++ lib/lbs-memory/main_memory.hpp
@@ -8,38 +8,78 @@
 #include <vector>
 #include <observer.hpp>
 
-namespace lbs
-{
-	
-	class MainMemory : public Observable<std::pair<uint32_t, uint32_t>>
-	{
-	
-	public:
-		MainMemory();
-		//TODO: implement these functions
-		//TODO: handle if samplepack is too big for flash!
-		//int getFreeSpacefromFlash();
-		//int getFreeSpacefromSD();
-		//bool isFitting(constr String& pack_name)
-		//bool flashEmpty()
-		static void init();
-		static MainMemory* instance();
-		static void eraseFlash();
-		static void transferSingleToFlash( const String& filepath );
-		static void loadSamplepack( const String& pack_name );
-		
-		static std::vector<String> getFilelistFromFlash();
-		static std::vector<String> getSampleNamesFromPack( const String& pack_name );
-		static std::vector<String> getSamplePacksFromSD();
-		
-		static void printAllFilesFromSD();
-		//static void transferFolderToFlash( const std::string& filepath );
-		//std::vector< String > getAllSamplepacks();
-		//std::vector<String> getAllFiles();
-		//std::vector<String> getAllSamples( const String& samplepack );
-		//void initializeSamplepack( const String& samplepack );
-	private:
-		const static String m_packRootDir;
-		static MainMemory* m_glue;
-	};
+namespace lbs {
+    /*enum AUDIOTYPE {
+        INVALID = 0,
+        WAV = 1,
+        RAW = 2
+    };*/
+
+/*
+    enum playbackMode {
+        ONESHOT = 0,
+        LOOP = 1,
+    };
+*/
+
+/*
+    struct PitchVal {
+        int8_t value = -1;
+    };
+*/
+
+/*
+    struct mapping {
+        String sample;
+        playbackMode mode;
+    };
+*/
+    class MainMemory : public Observable<std::pair<uint32_t, uint32_t>> {
+
+    public:
+        MainMemory();
+
+        static void init();
+
+        static MainMemory *instance();
+
+        static void printAllFilesFromSD();
+
+        //static uint64_t getFreeSpacefromSD();
+        static std::vector<String> getFilelistFromFlash();
+
+        //static bool flashEmpty();
+        static void eraseFlash();
+
+        static uint getFreeSpacefromFlash();
+
+        //static String getNoteName(uint8_t midiNote);
+        static std::vector<String> getSamplePacksFromSD();
+
+        static std::vector<String> getSampleNamesFromPack(const String &pack_name);
+
+        /**/static void loadSamplepack(const String &pack_name);
+        //static String getLoadedPackName();
+        //static String getSampleFromNote(uint8_t note);
+        //static bool setSampleForNote(const String &sampleName, uint8_t midiNote);
+
+        //static bool createStdMappingFile(const String &packName);
+        //static bool deleteMappingFile(const String &packName);
+        //static void createAllStdMappingFiles();
+        //static bool saveCurrentMappingToFile();
+        //static void printMapping();
+        /**/static void transferSingleToFlash(const String &filepath);
+
+
+    private:
+        const static String m_packRootDir;
+        static MainMemory *m_glue;
+        //static String currentPack;
+        //static uint freeSpaceFlash;
+        //static String sampleMapping[128];
+        //static playbackMode modeMapping[128];
+
+        //static void loadMappingFile(const String &packName);
+
+    };
 }// namespace lbs
